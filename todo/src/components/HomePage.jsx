01b83ceab63ed7465e49497753c2fd78bb18238f
adityarajbenn <adityarajbenn@gmail.com>
2023-02-08 00:37:12 +0530

    adding delete Button

--- todo/src/components/HomePage.jsx
+++ todo/src/components/HomePage.jsx
@@ -7,6 +7,9 @@ function HomePage() {
   const [input, setInput] = useState("");
   const [placeHolderText,setPlaceHolderText] = useState("Enter Text");
   const [editID,setEditID] = useState("");
+  const [deleteId,setDeleteId] = useState("");
+  const [div,setDiv] = useState(false);
+
   const list = useSelector((state) => state.list);
 
   const dispatch = useDispatch();
@@ -19,11 +22,28 @@ function HomePage() {
       setEditID(id);
   }
 
+  function deleteButton(){
+
+    dispatch(deleteTodo(deleteId));
+    setDiv(false);
+  }
+
   return (
     <>
-    <div id="mainDiv">
+    <div id="mainDiv" style={{position:"relative"}}>
+      {div? <div style={{position:"fixed"} } id="deletePermission">
+        <h3>Do you want to delete?</h3>
+        <div>
+          <button id="deleteYes" onClick={()=>deleteButton()}>Yes</button>
+          <button id="deleteNo" onClick={()=>setDiv(false)}>No</button>
+        </div> 
+      </div>: "" }
         <div id="contentDiv">
+          <div style={{display:"flex",color:"white",alignItems:"center",justifyContent:"space-around"}}>
 
+        <h3 >Add items to TODO-List</h3>
+        <i class="fa-solid fa-pen-to-square"></i>
+          </div>
       <div className="inputDiv">
         <input
           value={input}
@@ -36,12 +56,12 @@ function HomePage() {
           placeholder={placeHolderText} 
           />
           {editID!==""?
-          <i class="fa-solid fa-square-check" onClick={() => {
+          <i style={{color:"green",fontSize:"20px"}} class="fa-solid fa-square-check" onClick={() => {
               dispatch(editTodo(editID,input), setPlaceHolderText("Enter Text"),setEditID(""),setInput(""));
             }}></i>
         :
-          <i className="fa-solid fa-plus"  onClick={() => {
-              dispatch(addTodo(input), setInput(""));
+          <i style={{fontSize:"20px"}} className="fa-solid fa-plus"  onClick={() => {
+              dispatch(addTodo(input), setInput(true));
             }}
             ></i>
         }
@@ -55,12 +75,12 @@ function HomePage() {
                     display:"flex",
                     justifyContent:"space-between",
                     width:"50px",
-                    border:"none"
-
+                    border:"none",
+                    background:"none"
                 }}>
-                <i className="fa-solid fa-pencil" onClick={()=>{editFun(elem.id,elem.data)}}></i>
-                <i className="fa-solid fa-trash-can" onClick={() => {
-                    dispatch(deleteTodo(elem.id));
+                <i id="editButton" className="fa-solid fa-pencil" onClick={()=>{editFun(elem.id,elem.data)}}></i>
+                <i id="deleteButton" className="fa-solid fa-trash-can" onClick={() => {
+                  setDeleteId(elem.id) ; setDiv(true)
                 }}></i>
                 </div>
             </div>
